e9a3a66df9ba58b875266822106e071424167da8
Abhijeet Kamble <akamble@calibo.com>
2022-09-08 13:07:45 +0530

    added qa code

---
+++ demo-17/QA/provider.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+  cloud {
+        organization = "abhijeetka"
+    #
+        workspaces {
+          name = "tf-demo-qa"
+        }
+  }
+}
+
+
+provider "aws" {
+  region = var.aws_region
+  assume_role {
+    role_arn    = var.assume_role_name
+    external_id = "my_external_id"
+  }
+}
